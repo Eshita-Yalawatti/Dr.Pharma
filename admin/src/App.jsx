f9238a06d3805054185f9f7f42cf89f4ebe7fc9d
i-am-surovi <ssur.ovi03@gmail.com>
2025-09-05 23:08:06 +0600

    submitted

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
       <div className="flex items-start">
         <Sidebar />
         <Routes>
-          <Route path="/" element={<></>} />
+          <Route path="/" element={<Dashboard/>} />
           <Route path="/admin-dashboard" element={<Dashboard />} />
           <Route path="/all-appointments" element={<AllAppointments />} />
           <Route path="/all-orders" element={<AllOrders />} />
