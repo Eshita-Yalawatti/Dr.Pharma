3ad840782c925e21c0f2be7655c0eb60383148ad
Eshita-Yalawatti <eshitay12345@gmail.com>
2025-10-26 00:07:50 +0530

    Update Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -4,18 +4,21 @@ import { assets } from "../assets/assets/assets";
 const Contact = () => {
   return (
     <div>
+      {/* Header */}
       <div className="text-center text-2xl pt-10 text-[#707070]">
         <p>
           CONTACT <span className="text-gray-700 font-semibold">US</span>
         </p>
       </div>
 
+      {/* Main Section */}
       <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm">
         <img
           className="w-full md:max-w-[360px]"
           src={assets.contact_image}
-          alt=""
+          alt="Contact"
         />
+
         <div className="flex flex-col justify-center items-start gap-6">
           <p className="font-semibold text-lg text-gray-600">OUR OFFICE</p>
           <p className="text-gray-500">
@@ -23,9 +26,7 @@ const Contact = () => {
             <br />
             Kha 224, Bir Uttam Rafiqul Islam Ave, Dhaka 1212, BD
           </p>
-          <button className="text-blue-600 underline">
-            Explore Jobs
-          </button>
+          <button className="text-blue-600 underline">Explore Jobs</button>
         </div>
       </div>
     </div>
